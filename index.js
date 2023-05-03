53f53cf11796cff492702e1ca7e2b45fe360f0bf
Ayush Uprety <ayushuprety1@gmail.com>
2023-05-03 11:04:06 +0545

    Wrote a code for startgame and setscreen function

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const canvas = document.getElementById("snakecanva");
-const abc = canvas.getContext("2d");
+const canva = canvas.getContext("2d");
+let speed = 7;
+
+function startGame() {
+  setScreen();
+  setTimeout(startGame, 1000 / speed); // Rerender the screen 7 times a second
+}
+function setScreen() {
+  canva.fillStyle = "Brown"; //make screen brown
+  canva.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+}
